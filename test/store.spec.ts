9b24f86f587779ce66fb125c28c0868ee716acfe
arusakov <a_s_rusakov@mail.ru>
2017-01-13 23:34:39 +0300

    equal -> stringEqual

--- test/store.spec.ts
+++ test/store.spec.ts
@@ -1,4 +1,4 @@
-import { equal } from 'assert'
+import { strictEqual } from 'assert'
 
 import { selectLastVersion } from '../src/store'
 
@@ -8,7 +8,7 @@ describe('store', () => {
     const rowMock = {}
     const dbMock = {
       query: (sql) => {
-        equal(sql, 'SELECT * FROM versions' +
+        strictEqual(sql, 'SELECT * FROM versions' +
           ' ORDER BY version[1] DESC, version[2] DESC, version[3] DESC' +
           ' LIMIT 1;')
         return Promise.resolve({ rowCount: 1, rows: [rowMock] })
@@ -16,7 +16,7 @@ describe('store', () => {
     }
     const result = await selectLastVersion(dbMock as any, 3)
 
-    equal(result, rowMock)
+    strictEqual(result, rowMock)
   })
 
 })
